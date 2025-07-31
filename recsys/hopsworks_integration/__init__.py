7e21326edc21d4fe0855e77d7d58038e5cb57cf1
Visitxay Hanmonty <visitxay_hanmonty@berkeley.edu>
2025-07-30 19:23:00 -0700

    Create model registry and hopworks integration

--- recsys/hopsworks_integration/__init__.py
+++ recsys/hopsworks_integration/__init__.py
@@ -1,4 +1,4 @@
 from .feature_store import get_feature_store
-from . import feature_store, constants
+from . import feature_store, constants, two_tower_serving
 
-__all__ = ["get_feature_store", "feature_store", "constants"]
+__all__ = ["get_feature_store", "feature_store", "constants", "two_tower_serving"]
